bd6af4aa97f9ccb6098e3c023ef3ff1b82625c7d
Rafaelmorales1982 <rafaelmorconcursos@gmail.com>
2025-01-07 11:22:37 -0300

    Atualizei a classe MarcaProduto.java

--- src/main/java/jdev/mentoria/lojavirtual/model/MarcaProduto.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/MarcaProduto.java
@@ -9,14 +9,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
-
+//Pense nela como uma representação de algo que será armazenado no banco.
 @Entity
 
 
-
+//é uma anotação fornecida pela JPA que é usada para especificar detalhes sobre a tabela de banco de dados que será mapeada para uma entidade (classe Java).
 @Table(name = "marca_produto")
 
-
+// é uma anotação poderosa em JPA que permite utilizar sequências de banco de dados para gerar chaves primárias.
 @SequenceGenerator (name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
 public class MarcaProduto implements Serializable {
 
@@ -25,6 +25,8 @@ public class MarcaProduto implements Serializable {
 	
 	@Id
 	
+	
+	//Ela permite que você defina a estratégia de geração de IDs que melhor se adapta às necessidades da sua aplicação e do seu banco de dados, sem precisar gerá-los manualmente.
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
 	private Long id;
 	
