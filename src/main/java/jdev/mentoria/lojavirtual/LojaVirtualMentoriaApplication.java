212ec53a123a934b108d1a677c59c86a9a9d53fa
Rafaelmorales1982 <rafaelmorconcursos@gmail.com>
2025-01-13 00:28:22 -0300

    Atualizaçao

--- src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -2,8 +2,17 @@ package jdev.mentoria.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 
 @SpringBootApplication
+/*
+ @EntityScan é uma ferramenta essencial para configurar o Spring Data JPA e mapear as entidades corretamente 
+ no seu projeto, garantindo que as tabelas sejam criadas e que você possa realizar operações no banco de dados.
+  */
+
+
+@EntityScan(basePackages = "jdev.mentoria.lojavirtual.model")
 public class LojaVirtualMentoriaApplication {
 
 	public static void main(String[] args) {
